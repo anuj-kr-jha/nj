a22e53f6f87c400263be6fcf484f00b86a444c57
Anuj Kumar Jha <anuj.kr.jha@protonmail.com>
2023-10-14 05:43:34 +0530

    refactor

--- scripts/app.mjs
+++ scripts/app.mjs
@@ -1,7 +1,7 @@
 import { React, ReactDOM } from './react.mjs';
 
 // - creating element is core of react so done by React itself
-const heading = React.createElement('h1', { id: 'hading', xyz: 'abc' }, 'Hello World from react'); // <Tag, Obj, Value>
+const heading = React.createElement('h1', { id: 'heading', xyz: 'abc' }, 'Hello World from react'); // <Tag, Obj, Value>
 
 // - render element to DOM is done by ReactDOM
 const root = ReactDOM.createRoot(document.querySelector('#root'));

adbcf3915fb1992f6a3ed2630fe0ba5d326bb855
Anuj Kumar Jha <anuj.kr.jha@protonmail.com>
2023-10-14 05:36:28 +0530

    with react

---
+++ scripts/app.mjs
@@ -0,0 +1,9 @@
+import { React, ReactDOM } from './react.mjs';
+
+// - creating element is core of react so done by React itself
+const heading = React.createElement('h1', { id: 'hading', xyz: 'abc' }, 'Hello World from react'); // <Tag, Obj, Value>
+
+// - render element to DOM is done by ReactDOM
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
+root.render(heading);
---
+++ scripts/react.mjs
@@ -0,0 +1,9 @@
+/**
+ * @type {typeof import('react')}
+ */
+export const React = window.React;
+
+/**
+ * @type {typeof import('react-dom/client')}
+ */
+export const ReactDOM = window.ReactDOM;

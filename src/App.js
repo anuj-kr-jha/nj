3f9f93d5b3dce3e5d25b48f98d5388062a2082d0
Anuj Kumar Jha <anuj.kr.jha@protonmail.com>
2023-10-19 05:43:01 +0530

    assignment 3

--- src/App.js
+++ src/App.js
@@ -1,50 +1,55 @@
-/**
- * * REACT COMPONENT
- * ? 1. Function based component [new]
- * ? 2. Class based component [old]
- *
- */
+import React from 'react';
+import logo from '../public/react.jpg';
+import icon from '../public/user.jpg';
+// -------------WITHOUT JSX -------------
+const h1 = React.createElement('h1', {}, 'I am H1');
+const h2 = React.createElement('h2', {}, 'I am H2');
+const h3 = React.createElement('h3', {}, 'I am H3');
+const divWithoutJSX = React.createElement('div', { className: 'title' }, [h1, h2, h3]);
+// export default divWithoutJSX;
 
-/**
- * ? FUNCTION BASED COMPONENT
- * 1. It should be named with PascalCase ( first letter should be capital )
- * 2. It is a normal JS function which returns some React element/ JSX
- *
- * For rendering a component, we have to use root.render(<ComponentName />) instead of root.render(ReactElement)
- *
- */
-
-const TitleComponent = () => (
-  <h3 className="title" tabIndex="1">
-    This is title component
-  </h3>
-);
-
-const titleElement = (
-  <h3 className="title" tabIndex="2">
-    This is title element
-  </h3>
-);
-
-/**
- * * COMPONENT COMPOSITION
- * We can use one or more component inside another component
- */
-const HeadingComponent = () => (
-  <div class="container">
-    <h1 className="heading">This is heading component</h1>
-    <TitleComponent />
-    {titleElement}
+// ------------- WITH JSX -------------
+const divWithJSX = (
+  <div className="title">
+    <h1>I am H1</h1>
+    <h2>I am H2</h2>
+    <h3>I am H3</h3>
   </div>
 );
-export default HeadingComponent;
+// export default divWithJSX;
 
-/**
- * All of the following are valid for component:
- *   <TitleComponent />
- *   <TitleComponent></TitleComponent>
- *   {TitleComponent()}
- *
- * All of the following are valid for element:
- *  {titleElement}
- */
+// ------------- COMPONENT -------------
+const H1 = () => <h1>I am H1</h1>;
+const H2 = () => <h2>I am H2</h2>;
+const H3 = () => <h3>I am H3</h3>;
+
+const Logo = () => (
+  <div className="logo">
+    <img src={logo} alt="Logo" />
+  </div>
+);
+
+const SearchBar = () => (
+  <div className="search-bar">
+    <input type="text" placeholder="Search..." />
+  </div>
+);
+
+const UserIcon = () => (
+  <div className="user-icon">
+    <img src={icon} alt="user icon" />{' '}
+  </div>
+);
+
+const DivWithJSXAndComponents = () => (
+  <div className="title">
+    <Logo />
+    <SearchBar />
+    <UserIcon />
+    {/* <H1 /> */}
+    {/* <H2> </H2> */}
+    {/* {H3()} */}
+  </div>
+);
+
+export default <DivWithJSXAndComponents />;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import ParentComponent from './App.js';
+import parent from './App.js';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
-root.render(<ParentComponent />);
+root.render(parent);
